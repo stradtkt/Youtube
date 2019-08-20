6d4efb9e3e379450491f378bed43f982ff3a3f27
Kevin <stradtkt22@gmail.com>
2019-08-20 01:30:32 -0400

    Added youtube activity

---
+++ app/src/main/java/com/stradtman/youtube/YoutubeActivity.java
@@ -0,0 +1,28 @@
+package com.stradtman.youtube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+
+public class YoutubeActivity extends AppCompatActivity
+        implements YouTubePlayer.OnInitializedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+}
