7f090e563926abe67c1272bcb5c599712afb5345
Kevin <stradtkt22@gmail.com>
2019-08-20 19:34:44 -0400

    Added youtube api

--- app/src/main/java/com/stradtman/youtube/YoutubeActivity.java
+++ app/src/main/java/com/stradtman/youtube/YoutubeActivity.java
@@ -1,19 +1,40 @@
 package com.stradtman.youtube;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 
 public class YoutubeActivity extends AppCompatActivity
         implements YouTubePlayer.OnInitializedListener {
 
+    static final String GOOGLE_API_KEY = "AIzaSyDQ7l9rxaluzCO2uvdlu_y5tJ5kIDqG4IA";
+    static final String YOUTUBE_VIDEO_ID = "LWs4porNorQ";
+    static final String YOUTUBE_PLAYLIST = "PLFlj2LIMhKOFber1Zqr0rjCbXRa0ptx5s";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+//        setContentView(R.layout.activity_youtube);
+//        ConstraintLayout constraintLayout = (ConstraintLayout)findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout)getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300,80));
+//        button1.setText("Button Added");
+//        layout.addView(button1);
+
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(player);
     }
 
     @Override
